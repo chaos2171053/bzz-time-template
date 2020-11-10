792087a910b9a44a71a9b6aaea6cfc2e96b08e36
qinliangchao <qinliangchao@timework.cn>
2020-11-10 11:35:57 +0800

    [ADD]create component style

--- src/generator/CreateComponent.ts
+++ src/generator/CreateComponent.ts
@@ -7,7 +7,7 @@ import createFile from './createFile';
 
 export default class CreateComponent {
   // 必须把 templates 目录放到项目根目录。否则 templates 下的文件加载时不会被读取到。
-  public static assetRootDir: string = path.join(__dirname, "../../templates/ComponentName.js");
+  public static assetRootDir: string = path.join(__dirname, "../../templates/ComponentName");
   componentName: string = "";
   uri;
   constructor(uri: Uri, componentName: string) {
@@ -27,21 +27,25 @@ export default class CreateComponent {
     return componentDir;
   }
   public async createComponentFile(componentDir:string) {
-    let componentContent = '';
-    let file  = null;
+    let componentJsContent = '';
+    let componentStyleContent = '';
+    let jsFile  = null;
+    let styleFile =null
     try {
-      componentContent = fse.readFileSync(CreateComponent.assetRootDir,'utf8').toString();     
+      componentJsContent = fse.readFileSync(`${CreateComponent.assetRootDir}.js`,'utf8').toString(); 
+      componentStyleContent = fse.readFileSync(`${CreateComponent.assetRootDir}.less`,'utf8').toString();  
     } catch (error) {
       throw new Error(error);
       
     }
 
-    componentContent = componentContent.replace(/ComponentName/g,this.componentName);
+    componentJsContent = componentJsContent.replace(/ComponentName/g,this.componentName);
     try {
-      file = await createFile(`${componentDir}/${this.componentName}.js`,componentContent);
+      jsFile = await createFile(`${componentDir}/${this.componentName}.js`,componentJsContent);
+      styleFile = await createFile(`${componentDir}/${this.componentName}.less`,componentStyleContent);
     } catch (error) {
       throw new Error(error);
     }
-    return file;
+    return true;
   }
 }
